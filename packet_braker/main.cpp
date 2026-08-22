f83832f07fa82ee13ecdfcc04b187630ec10f8cc
dukebismaya <179898597+dukebismaya@users.noreply.github.com>
2026-08-22 22:36:26 +0530

    added player and ball class

--- packet_braker/main.cpp
+++ packet_braker/main.cpp
@@ -2,9 +2,39 @@
 
 constexpr int SCREEN_HEIGHT = 450;
 constexpr int SCREEN_WIDTH = 800;
+constexpr int PLAYER_MAX_LIFE = 5;
+
+class Player {
+public:
+  Vector2 position;
+  Vector2 size;
+  int life;
+  Player() {
+    size = {(float)SCREEN_WIDTH / 10, 20};
+    position = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT * 7 / 8};
+    life = PLAYER_MAX_LIFE;
+  }
+};
+
+class Ball {
+public:
+  Vector2 position;
+  Vector2 speed;
+  int radius;
+  bool active;
+
+  Ball() {
+    position = {0, 0};
+    speed = {0, 0};
+    radius = 7;
+    active = false;
+  }
+};
 
 class BismayaEngine {
 private:
+  Player player;
+  Ball ball;
   bool gameOver;
   bool pause;
 
@@ -19,9 +49,13 @@ public:
 
   void run() {
     while (!WindowShouldClose()) {
-      // Update();
+      Update();
       draw();
     }
+  }
+
+  void Update() {
+    // I will write later.
   }
 
   void draw() {
