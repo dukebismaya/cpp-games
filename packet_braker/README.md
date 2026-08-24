fdd3f927da757ac685c549a95dd47594eb717e9b
dukebismaya <179898597+dukebismaya@users.noreply.github.com>
2026-08-24 15:13:22 +0530

    update: game state with bricks initialization and collision tracking

--- packet_braker/README.md
+++ packet_braker/README.md
@@ -1,1 +1,5 @@
-![Preview Image 1](image.png)
+### Preview Images
+
+![Preview Image 1](GAME_START.png)
+![Preview Image 2](PLAYING_STATE.png)
+![Preview Image 3](GAME_OVER.png)
--- packet_braker/main.cpp
+++ packet_braker/main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <cstddef>
+#include <format>
 #include <string>
 
 constexpr int SCREEN_HEIGHT = 450;
@@ -7,6 +8,7 @@ constexpr int SCREEN_WIDTH = 800;
 constexpr int PLAYER_MAX_LIFE = 5;
 constexpr int LINES_OF_BRICKS = 5;
 constexpr int BRICKS_PER_LINE = 20;
+int player_curr_life = PLAYER_MAX_LIFE;
 
 class Player {
 public:
@@ -30,7 +32,7 @@ public:
   Ball() {
     position = {0, 0};
     speed = {0, 0};
-    radius = 7;
+    radius = 10;
     active = false;
   }
 };
@@ -129,6 +131,7 @@ public:
           ball.speed = {0, 0};
           ball.active = false;
           player.life--;
+          player_curr_life--;
           if (player.life <= 0)
             gameOver = true;
         }
@@ -146,6 +149,32 @@ public:
                            (player.size.x / 2) * 5.0f;
           }
         }
+        int activeBricks{};
+        for (int i = 0; i < LINES_OF_BRICKS; ++i) {
+          for (int j = 0; j < BRICKS_PER_LINE; ++j) {
+            auto &curr_brick = brick[i][j];
+            if (curr_brick.active) {
+              activeBricks++;
+              Rectangle brickRec = {curr_brick.position.x - brickSize.x / 2,
+                                    curr_brick.position.y - brickSize.y / 2,
+                                    brickSize.x, brickSize.y};
+              if (CheckCollisionCircleRec(ball.position,
+                                          static_cast<float>(ball.radius),
+                                          brickRec)) {
+                curr_brick.active = false;
+                if (ball.position.y <= brickRec.y ||
+                    ball.position.y >= brickRec.y + brickRec.height) {
+                  ball.speed.y *= -1.0f;
+                } else {
+                  ball.speed.x *= -1.0f;
+                }
+              }
+            }
+          }
+        }
+        if (activeBricks <= 0) {
+          gameOver = true;
+        }
       }
     }
   }
@@ -156,14 +185,14 @@ public:
     DrawRectangle((int)(player.position.x - player.size.x / 2),
                   (int)(player.position.y - player.size.y / 2),
                   (int)player.size.x, (int)player.size.y, WHITE);
-    DrawCircleV(ball.position, static_cast<float>(ball.radius), BLUE);
+    DrawCircleV(ball.position, static_cast<float>(ball.radius), RED);
 
     // Drawing Bricks
     for (int i = 0; i < LINES_OF_BRICKS; ++i) {
       for (int j = 0; j < BRICKS_PER_LINE; ++j) {
         const auto &curr_brick = brick[i][j];
         if (curr_brick.active) {
-          Color brickColor = ((i + j) % 2 == 0) ? GRAY : DARKGRAY;
+          Color brickColor = ((i + j) % 2 == 0) ? GREEN : DARKGRAY;
           DrawRectangle(
               static_cast<int>(curr_brick.position.x - brickSize.x / 2),
               static_cast<int>(curr_brick.position.y - brickSize.y / 2),
@@ -177,6 +206,9 @@ public:
       std::string instruction{"Press SPACE to launch the ball"};
       DrawText(instruction.c_str(), (SCREEN_WIDTH / 2) - 150,
                SCREEN_HEIGHT - 20, 20, DARKGRAY);
+    } else {
+      DrawText(std::format("{}/{}", player_curr_life, PLAYER_MAX_LIFE).c_str(),
+               (SCREEN_WIDTH / 2), SCREEN_HEIGHT - 20, 20, DARKGRAY);
     }
     EndDrawing();
   }
